f53657b33d88d7bdc6e70fe6c5006283a20d19cb
Joe Heywood <joe.heywood@ncvo.org.uk>
2016-02-28 22:27:43 +0000

    In progress: inheritence in rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,15 +5,15 @@
 
 using namespace Rcpp;
 
-// rollingMedian3
-NumericVector rollingMedian3(NumericVector v, int k);
-RcppExport SEXP rapidRoll_rollingMedian3(SEXP vSEXP, SEXP kSEXP) {
+// rollingMedianRaw
+NumericVector rollingMedianRaw(NumericVector v, int k);
+RcppExport SEXP rapidRoll_rollingMedianRaw(SEXP vSEXP, SEXP kSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
     Rcpp::traits::input_parameter< NumericVector >::type v(vSEXP);
     Rcpp::traits::input_parameter< int >::type k(kSEXP);
-    __result = Rcpp::wrap(rollingMedian3(v, k));
+    __result = Rcpp::wrap(rollingMedianRaw(v, k));
     return __result;
 END_RCPP
 }
--- src/rollMedDev3.cpp
+++ src/rollMedDev3.cpp
@@ -3,12 +3,12 @@ using namespace Rcpp;
 #include <vector>
 using namespace std;
 
-class Window { 
+class Window {
 private:
     vector<double> currentArray;
     vector<int> order;
     int k, midVal, lowest, highest;
-    
+
     void swapValues(int v1, int v2) {
         currentArray[v1] = currentArray[v1] + currentArray[v2];
         currentArray[v2] = currentArray[v1] - currentArray[v2];
@@ -17,14 +17,14 @@ private:
         order[v2] = order[v1] - order[v2];
         order[v1] = order[v1] - order[v2];
     }
-    
+
     void sortDown(int j) {
         while (j > 0 && currentArray[j] < currentArray[j-1]) {
             swapValues(j, j - 1);
             j--;
         }
     }
-    
+
     void sortUp(int j) {
         while(j < (k - 1) && currentArray[j] > currentArray[j + 1]) {
             swapValues(j, j + 1);
@@ -44,7 +44,7 @@ public:
         lowest = 0;
         highest = k - 1;
     }
-    
+
     double addValue(double v) {
         int l = 0;
         while(order[l] > lowest) l++;
@@ -55,7 +55,7 @@ public:
         if(l < (k - 1) && v > currentArray[l + 1]) sortUp(l);
         return currentArray[midVal];
     }
-    
+
     double initialSort() {
         for(int i = 0; i < k; i++) {
             sortDown(i);
@@ -65,16 +65,13 @@ public:
 };
 
 // [[Rcpp::export]]
-NumericVector rollingMedian3(NumericVector v, int k) {
-    vector<double> firstK (k); 
-    NumericVector out(v.size());
+NumericVector rollingMedianRaw(NumericVector v, int k) {
+    vector<double> firstK (k);
+    NumericVector out(v.size() - (k-1) ) ;
     for(int i = 0; i < k; i++) firstK[i] = v[i];
     Window w(firstK);
     int oi = 0;
     out[oi++] = w.initialSort();
-    while(oi < ((k+1)/2)) out[oi++] = out[0];
     for(int j = k; j < v.size(); j++) out[oi++] = w.addValue(v[j]);
-    double last = out[oi-1];
-    while(oi < v.size()) out[oi++] = last;
     return out;
 }

d5b2b3a52585057ae3140d4e8d475cc09eadf9c8
Joe Heywood <joe.heywood@ncvo.org.uk>
2016-02-29 23:12:09 +0000

    small changes

--- src/rollAverage.cpp
+++ src/rollAverage.cpp
@@ -5,14 +5,13 @@ using namespace std;
 
 class RollingAveWindow {
 private:
-    RollingAveWindow();    
+    RollingAveWindow();
 protected:
     int k, lowest;
     vector<double> currentArray;
     vector<int> order;
     RollingAveWindow(vector<double> firstK) {
         k = firstK.size();
-        //midVal = (k - 1) / 2;
         currentArray = firstK;
         order.reserve(k);
         for(int i = 0; i < k; i++) {
@@ -20,56 +19,83 @@ protected:
         }
         lowest = 0;
     }
-
-    //double addValue(double v);
-    //double initialValue();
-public:
-
 };
-
 
 class RollingMeanWindow: public RollingAveWindow {
 private:
     double sum;
 public:
     RollingMeanWindow(vector<double> a) : RollingAveWindow(a) {}
-    double initialValue(); 
-    double addValue(double v);
+    double initialValue() {
+        sum = 0;
+        for(int i = 0; i < k; i++) {
+            sum += currentArray[i];
+        }
+        return sum / k;
+    }
+    double addValue(double v){
+        sum += (v - currentArray[lowest]);
+        currentArray[lowest] = v;
+        if(lowest == (k-1)) {
+            lowest = 0;
+        } else{
+            lowest++;
+        }
+        return sum / k;
+    }
 };
 
 class RollingMedianWindow: public RollingAveWindow {
 private:
     int midVal, highest;
+    void swapValues(int v1, int v2) {
+        currentArray[v1] = currentArray[v1] + currentArray[v2];
+        currentArray[v2] = currentArray[v1] - currentArray[v2];
+        currentArray[v1] = currentArray[v1] - currentArray[v2];
+        order[v1] = order[v1] + order[v2];
+        order[v2] = order[v1] - order[v2];
+        order[v1] = order[v1] - order[v2];
+    }
+
+    void sortDown(int j) {
+        while (j > 0 && currentArray[j] < currentArray[j-1]) {
+            swapValues(j, j - 1);
+            j--;
+        }
+    }
+
+    void sortUp(int j) {
+        while(j < (k - 1) && currentArray[j] > currentArray[j + 1]) {
+            swapValues(j, j + 1);
+            j++;
+        }
+    }
 public:
     RollingMedianWindow(vector<double> a) : RollingAveWindow(a) {}
-    double initialValue();
-    double addValue(double v);
+    double initialValue() {
+        midVal = (k - 1) / 2;
+        highest = k - 1;
+        for(int i = 0; i < k; i++) {
+            sortDown(i);
+        }
+        return currentArray[midVal];
+    }
+    double addValue(double v){
+        int l = 0;
+        while(order[l] > lowest) l++;
+        currentArray[l] = v;
+        order[l] = ++highest;
+        lowest++;
+        if(l > 0 && v < currentArray[l - 1]) sortDown(l);
+        if(l < (k - 1) && v > currentArray[l + 1]) sortUp(l);
+        return currentArray[midVal];
+    }
 };
 
 
 
-double RollingMeanWindow::initialValue() {
-    sum = 0;
-    for(int i = 0; i < k; i++) {
-        sum += currentArray[i];
-    }
-    return sum / k;
-}
-
-double RollingMeanWindow::addValue(double v) {
-    sum += (v - currentArray[lowest]);
-    currentArray[lowest] = v;
-    if(lowest == (k-1)) {
-        lowest = 0;
-    } else{
-        lowest++;
-    }
-    return sum / k;
-}
-
-
 // [[Rcpp::export]]
-NumericVector rollingMeanRawX(NumericVector v, int k) {
+NumericVector rollingMeanRaw(NumericVector v, int k) {
     vector<double> firstK (k);
     NumericVector out(v.size() - (k-1) ) ;
     for(int i = 0; i < k; i++) firstK[i] = v[i];
@@ -80,13 +106,14 @@ NumericVector rollingMeanRawX(NumericVector v, int k) {
     return out;
 }
 
-//NumericVector rollingMedianRaw(NumericVector v, int k) {
-//    vector<double> firstK (k);
-//    NumericVector out(v.size() - (k-1) ) ;
-//    for(int i = 0; i < k; i++) firstK[i] = v[i];
-//    RollingMedianWindow w(firstK);
-//    int oi = 0;
-//    out[oi++] = w.initialValue();
-//    for(int j = k; j < v.size(); j++) out[oi++] = w.addValue(v[j]);
-//    return out;
-//}
+// [[Rcpp::export]]
+NumericVector rollingMedianRaw(NumericVector v, int k) {
+    vector<double> firstK (k);
+    NumericVector out(v.size() - (k-1));
+    for(int i = 0; i < k; i++) firstK[i] = v[i];
+    RollingMedianWindow w(firstK);
+    int oi = 0;
+    out[oi++] = w.initialValue();
+    for(int j = k; j < v.size(); j++) out[oi++] = w.addValue(v[j]);
+    return out;
+}

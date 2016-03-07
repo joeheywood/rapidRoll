5200673c80c94424b410c41e27bd6bcc6a431f2d
Joe Heywood <joe.heywood@ncvo.org.uk>
2016-03-07 07:57:34 +0000

    end of weekend save

--- R/rapidRoll.R
+++ R/rapidRoll.R
@@ -2,33 +2,35 @@
 ## Main functions
 ################################################################################
 
-rapidRollMean <- function(unrolled, winSize = 3, pad = "NA", keyVals = NULL) {
+rapidRollMean <- function(unrolled, winSize = 3, pad = "none") {
+    rollingAveRaw(unrolled, 3, "mean")
+}
+
+rapidRollMedian <- function(unrolled, winSize = 3, pad = "none") {
+    rollingAveRaw(unrolled, 3, "median")
+}
+
+rapidRollToDF <- function(unrDF, col, rrFun = rapidRollMean, winSize = 3,
+                          pad = "none", align = "centre") {
+    print(paste0("Col: ", col, " | ", is.numeric(col)))
+    newColName <- paste0(ifelse(is.numeric(col), colnames(unrDF)[col], col),
+                         "_rolled")
+    unrDF[,newColName] <- setPadding(rrFun(unrDF[, col], winSize), pad, align,
+                                     winSize)
+    unrDF
 
 }
 
-
-rapidRollMedian <- function(unrolled, winSize = 3, pad = "none", keyVals = NULL) {
-    rw <- rollingMedianRaw(unrolled, winSize)
-    if(pad == "none") {
-        rw <- rw
+setPadding <- function(x, pad, align, winSize) {
+    padSize = (winSize - 1) / 2
+    if(pad == "none"){
+        x <- c(rep(NA, padSize), x, rep(NA, padSize))
     } else if(is.numeric(pad)) {
-        rw <- c(rep(pad, (k-1)/2), rw, rep(pad, (k-1)/2))
-    } else if(pad == "na") {
-        rw <- c(rep(NA, (k-1)/2), rw, rep(NA, (k-1)/2))
-    } else if(pad == "firstLast") {
-        rw <- c(rep(rw[1], (k-1)/2), rw, rep(rw[length(rw)], (k-1)/2))
+        if(length(pad) == 1) pad[2] <- pad[1]
+        x <- c(rep(pad[1], padSize), x, rep(pad[2], padSize))
+    } else if(pad == "topTail") {
+        x <- c(rep(x[1], padSize), x, rep(x[length(x)], padSize))
     }
-    if(is.null(keyVals)){
-        rw
-    } else {
-        if(length(keyVals == length(rw))){
-            data.frame(key = keyVals, value = rw)
-        } else {
-            data.frame(key = keyVals, value = rw)
-        }
-
-    }
+    x
 }
-
-# rollingMax, rollingMin..?
 
